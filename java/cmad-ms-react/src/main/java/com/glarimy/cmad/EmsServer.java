1ff34c4979a54f2425b12eee42ec6957c7113cec
ruchi <ruagrawa@cisco.com>
2020-02-10 14:53:16 +0530

    change1

--- java/cmad-ms-react/src/main/java/com/glarimy/cmad/EmsServer.java
+++ java/cmad-ms-react/src/main/java/com/glarimy/cmad/EmsServer.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmsServer {
 	public static void main(String[] args) {
+		System.out.printl(" RUNNING THE EM SERVER====");
 		SpringApplication.run(EmsServer.class, args);
 	}
 }
