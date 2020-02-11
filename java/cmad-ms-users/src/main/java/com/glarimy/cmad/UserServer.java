1610ac98f58eaae6d9c79d8524e680c5a6fa8866
ruchi <ruagrawa@cisco.com>
2020-02-11 09:23:33 +0530

    added newLine

--- java/cmad-ms-users/src/main/java/com/glarimy/cmad/UserServer.java
+++ java/cmad-ms-users/src/main/java/com/glarimy/cmad/UserServer.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServer {
 	public static void main(String[] args) {
+			
 		SpringApplication.run(UserServer.class, args);
 	}
 }
